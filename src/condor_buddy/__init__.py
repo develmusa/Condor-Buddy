6cd41aabdccd33c9c06a4fbeb47209df7d4d6882
develmusa <contact@develmusa.digital>
2024-12-07 17:47:18 +0100

    added discord.py library with bot example

--- src/condor_buddy/__init__.py
+++ src/condor_buddy/__init__.py
@@ -1,2 +1,15 @@
 def hello() -> str:
     return "Hello from condor-buddy!"
+
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix='>', intents=intents)
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send('pong')
+
+bot.run('token')
